c3b0c340e6ee1e5580cce475340d3f3001118c65
ERIZOBOT <160595131+ERISOBONNI@users.noreply.github.com>
2024-03-05 12:57:32 -0300

    burrito

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,13 +5,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `🤗 𝑩𝒊𝒆𝒏𝒗𝒆𝒏𝒊𝒅𝒐𝒔 𝒂 𝑵𝒊𝒈𝒕𝒉𝒎𝒂𝒓𝒆𝒔 𝑺𝒖𝒓. 𝑳𝒂 𝒍𝒊𝒅𝒆𝒓 𝑩𝒐𝒏𝒏𝒊💕 𝒍𝒐𝒔 𝒔𝒂𝒍𝒖𝒅𝒂 ✨
-  𝐃𝐞𝐬𝐩𝐢𝐞𝐫𝐭𝐞𝐧 𝐛𝐨𝐭𝐜𝐢𝐭𝐨𝐬 🤖 𝐍𝐢𝐠𝐭𝐡𝐦𝐚𝐫𝐞𝐬 𝐥𝐨𝐬 𝐢𝐧𝐯𝐨𝐜𝐚𝐧 😉  : ${pesan}`;
+  const oi = `🤗 𝐌𝐢 𝐚𝐦𝐨𝐫... 𝐄𝐬 𝐪𝐮𝐞 𝐮𝐬𝐭𝐞𝐝 𝐬𝐞 𝐚𝐥𝐞𝐣𝐨 𝐦𝐮𝐜𝐡𝐨★𝐲 𝐲𝐨 𝐝𝐞 𝐥𝐞𝐣𝐨𝐬 𝐧𝐨 𝐯𝐞𝐨 𝐛𝐞𝐛𝐞́★ 🤖 𝐍𝐢𝐠𝐭𝐡𝐦𝐚𝐫𝐞𝐬 𝐥𝐨𝐬 𝐢𝐧𝐯𝐨𝐜𝐚𝐧 😉  : ${pesan}`;
   let teks = `*💜💚Creador Wa.me/5493487548846 SI QUIERES BOT COMUNICAME💚💜*\n\n❏ ${oi}\n\n❏ *𝙼𝚎𝚗𝚌𝚒𝚘𝚗 𝚐𝚎𝚗𝚎𝚛𝚊𝚕.*\n`;
   for (const mem of participants) {
     teks += `🐛🦋 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└👑𝙱𝚈 𝙱𝙾𝙽𝙽𝙸 𝙱𝙴𝙱𝙴́👑\n\n`;
+  teks += `└👑𝐘 𝐝𝐚𝐦𝐞 𝐝𝐚𝐦𝐞 𝐝𝐚𝐦𝐞 𝐭𝐨𝐝𝐚 𝐭𝐮 𝐝𝐮𝐥𝐳𝐮𝐫𝐚 🎶\n\n`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
